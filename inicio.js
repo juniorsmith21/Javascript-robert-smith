7d5130cecba1d91c5bbd3262f6e71e70d7a4af20
vector5 <jhonalex9214@gmail.com>
2025-08-02 11:02:45 -0500

    Subiendo archivos

---
+++ inicio.js
@@ -0,0 +1,10 @@
+let numero = 25;
+console.log("El valor del numero es: " + numero);
+let nombre = "Robert Smith Ordoñez";
+let edad = 21;
+let sueldo=10000;
+let hijos =0;
+console.log("Yo me llamo: " +nombre+ "\nTengo: " + edad+ "\nAspiro ganar: " + sueldo+ "\nQuiero tener:  "+hijos+ " hijos");
+let masculino=true;
+let femenino=false;
+console.log("Mi genero es: "+ masculino+ " masculino ")
