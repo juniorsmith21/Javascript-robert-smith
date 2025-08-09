a98d0af8ec4af472bf88bcf4cb0dd7657da6ef4a
juniorsmith21 <ordonezjun0@gmail.com>
2025-08-09 11:57:18 -0500

    tercero

---
+++ alerta_multi.js
@@ -0,0 +1,5 @@
+let multi1=6;
+var multi2=3;
+let multiplicacion=multi1*multi2;
+console.log("El resultado de la multiplicacion es: "+ multiplicacion);
+alert("El resultado de la multiplicacion es: "+ multiplicacion);
---
+++ promt.js
@@ -0,0 +1,7 @@
+let numero1 =prompt("Digite el numero 1 a sumar");
+numero1 = parseInt(numero1);
+let numero2=prompt("Digite el numero 2 a sumar");
+numero2 = parseInt(numero2)
+let concatenar= numero1+numero2;
+console.log(concatenar);
+alert("Numeros concatenados: "+ concatenar)
