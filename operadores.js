55a7ca34c68de7f424520096032e01ac020e87bf
juniorsmith21 <ordonezjun0@gmail.com>
2025-08-09 09:18:08 -0500

    tercero

---
+++ operadores.js
@@ -0,0 +1,35 @@
+//suma, ressta,multiplicacion, division
+//calcular valores de dos o mas elementos
+//suma
+let num1=8
+let num2=5
+let suma=num1+num2
+console.log("El resultado es: "+suma);
+//contadores
+let edad=24;
+edad=edad+1 //edad++ remplaza
+console.log("Tu edad es: " + edad)
+//resta
+let res1=25;
+let res2=10;
+let resta=res1-res2;
+console.log("Resultado de la resta es: "+resta);
+//contador de resta
+let velocidad=50;
+velocidad=velocidad-4;
+console.log("La velocidad ahora es: " +velocidad);
+//multiplicacion
+let mult1=5
+let mult2=5
+let multiplicacion=mult1*mult2;
+console.log("El resultado de la multiplicaion es: "+multiplicacion)
+// division
+let div1=25;
+let div2=6;
+let division=div1/div2;
+console.log("El resultado de la division es: "+division)
+//residuo
+let resi1=20;
+let resi2=3;
+let residuo=resi1%resi2;
+console.log("Resultado del residuo es: " + residuo)
---
+++ variable.js
@@ -0,0 +1,19 @@
+//string
+var sena="ADSO";
+console.log(sena);
+//numeros
+let numeroEntero=17;
+var numeroNegativo=-4;
+let numeroDecimal=11.74;
+//boleanos true false
+var estoyEstudiando=true;
+var estoyAburrido=false;
+//undefine 
+var num;
+console.log(num);
+num=0;
+console.log(num)
+//null
+var sinNada=null;
+console.log(sinNada);
+
