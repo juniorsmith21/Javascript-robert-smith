ce43f748ae122f977460595c128497f5916178b3
juniorsmith21 <ordonezjun0@gmail.com>
2025-08-16 10:22:13 -0500

    16/08/25

--- numeroDivisible.js
+++ numeroDivisible.js
@@ -1,7 +1,7 @@
 let numero=parseFloat(prompt("Ingrese el numero para confirmar si es divisible"))
 let divisible=numero%3==0 && numero%5==0;
 if (divisible){
-    alert("El numero es divisible");
+    alert("El numero es divisible ");
 }
 else{
     alert("El numero no es divisible");
