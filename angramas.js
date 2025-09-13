b399009388075f5c5e140b83c5f8bb334838901c
juniorsmith21 <ordonezjun0@gmail.com>
2025-09-12 21:23:00 -0500

    12/09/2025

---
+++ angramas.js
@@ -0,0 +1,13 @@
+function anagrama(){
+    let smith=["a","m","o","r"]
+    respuesta=[]
+    for (let i=3;i>=0;i--){
+        for(let j=0;j<=3;j++){
+respuesta[j]=smith[i]
+        
+i=i-1
+    }
+}
+console.log(respuesta)
+}
+anagrama(); 
