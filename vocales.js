cd9ed24c7cbe65214cb983b70db961acf2040d26
juniorsmith21 <ordonezjun0@gmail.com>
2025-09-12 20:35:01 -0500

    12/09/25

--- vocales.js
+++ vocales.js
@@ -1,4 +1,15 @@
-function buscar(vocal){
-let vocales = [a,e,i,o,u,]
-for(let i=0)
+function buscar(vocalB){
+let vocales = ["a","e","i","o","u",]
+for(let i=0;i<vocales.length;i++){
+    if(vocalB==vocales[i]){
+        alert("Vocal encontrada "+ vocalB);
+    }
+    else{
+    }
+    alert("No existe")
 }
+}
+let B = prompt("Digite la palabra a buscar");
+
+buscar(B);
+
