fddeb3f071c1419212ceeed057880aee025a0fef
juniorsmith21 <ordonezjun0@gmail.com>
2025-09-12 20:05:59 -0500

    nuevo

---
+++ sumaArreglo.js
@@ -0,0 +1,11 @@
+function sumaArreglo() {
+let numero=[3,7,9,9,2]
+let numero1=[2,5,7,3,9]
+respuesta=[]
+for (let i=0;i<numero.length;i++)
+{
+respuesta[i]=numero[i]+numero1[i]
+}
+    console.log(respuesta)
+}
+sumaArreglo();
---
+++ vocales.js
@@ -0,0 +1,4 @@
+function buscar(vocal){
+let vocales = [a,e,i,o,u,]
+for(let i=0)
+}
